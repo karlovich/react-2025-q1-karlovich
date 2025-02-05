d0e6f9935d10d4f5a8f725dd1250359b12609d08
karlovich <alexkarlovich+git@gmail.com>
2025-02-06 01:21:22 +0300

    refactor: make SearchResults functional component

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Card } from './Card';
 import { Character } from '../shared/types';
 import { Loader } from './Loader';
@@ -9,88 +9,67 @@ interface SearchResultsProps {
   showError: boolean;
 }
 
-interface SearchResultsState {
-  count: number;
-  results: Character[];
-  loading: boolean;
-  error: boolean;
-}
+export const SearchResults = ({
+  searchTerm,
+  showError,
+}: SearchResultsProps) => {
+  const [count, setCount] = useState(0);
+  const [results, setResults] = useState<Character[]>([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-export class SearchResults extends Component<
-  SearchResultsProps,
-  SearchResultsState
-> {
-  state: SearchResultsState = {
-    count: 0,
-    results: [],
-    loading: true,
-    error: false,
-  };
-
-  componentDidMount() {
-    this.fetchData(this.props.searchTerm);
-  }
-
-  componentDidUpdate(prevProps: SearchResultsProps) {
-    if (this.props.showError) {
-      throw new Error('May the 4th be with u');
-    }
-    if (prevProps.searchTerm != this.props.searchTerm) {
-      this.setState({ error: false }, () => {
-        this.fetchData(this.props.searchTerm);
-      });
-    }
-  }
-
-  async fetchData(text: string) {
+  const fetchData = async (text: string) => {
     try {
-      this.setState({ loading: true });
+      setLoading(true);
 
       const response = await fetch(
         `https://swapi.dev/api/people/?search=${text.trim()}`
       );
 
       const data = await response.json();
-
-      this.setState({
-        count: data.count,
-        results: data.results,
-        loading: false,
-      });
+      setCount(data.count);
+      setResults(data.results);
+      setLoading(false);
     } catch {
-      this.setState({
-        error: true,
-        loading: false,
-      });
+      setError(true);
+      setLoading(false);
     }
+  };
+
+  useEffect(() => {
+    fetchData(searchTerm);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    if (showError) {
+      throw new Error('May the 4th be with u');
+    }
+  }, [showError]);
+
+  if (error) {
+    return <SearchFallback />;
   }
 
-  render() {
-    if (this.state.error) {
-      return <SearchFallback />;
-    }
-
-    return (
-      <div className="p-4">
-        <div className="mb-4">
-          <h1 className="text-xl font-bold text-black">
-            {this.state.count > 0
-              ? `Search of the Galactic Republic found ${this.state.count} creatures`
-              : 'No Creatures Found'}
-          </h1>
-        </div>
-        <div>
-          {this.state.loading ? (
-            <Loader />
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-              {this.state.results.map((character) => (
-                <Card key={character.url} character={character} />
-              ))}
-            </div>
-          )}
-        </div>
+  return (
+    <div className="p-4">
+      <div className="mb-4">
+        <h1 className="text-xl font-bold text-black">
+          {count > 0
+            ? `Search of the Galactic Republic found ${count} creatures`
+            : 'No Creatures Found'}
+        </h1>
       </div>
-    );
-  }
-}
+      <div>
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {results.map((character) => (
+              <Card key={character.url} character={character} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
