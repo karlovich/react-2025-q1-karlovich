1de55f65fea497cd219fdecd3176535fac6f57d0
karlovich <alexkarlovich+git@gmail.com>
2025-02-08 23:04:26 +0300

    feat: add test ability and coverage and implement 1st test

---
+++ src/components/Loader/Loader.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import { beforeEach, describe, it, expect } from 'vitest';
+import '@testing-library/jest-dom';
+import { Loader } from './Loader';
+import fadeCircles from '../../assets/fade-stagger-circles.svg';
+
+let loaderImg: HTMLElement;
+
+describe('Renders Loader Correctly', () => {
+  beforeEach(() => {
+    render(<Loader />);
+    loaderImg = screen.getByTestId('test-loader-img');
+  });
+
+  it('has correct image src and className', () => {
+    expect(loaderImg).toBeVisible();
+    expect(loaderImg).toHaveAttribute('src', fadeCircles);
+    expect(loaderImg).toHaveClass('loader-img');
+  });
+
+  it('image parent has correct className', () => {
+    render(<Loader />);
+    const parent = loaderImg.parentElement;
+    expect(parent).toHaveClass('loader');
+  });
+});
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,7 +4,12 @@ import fadeCircles from '../../assets/fade-stagger-circles.svg';
 export const Loader = () => {
   return (
     <div className="loader">
-      <img src={fadeCircles} className="loader-img" alt="Loading..." />
+      <img
+        src={fadeCircles}
+        className="loader-img"
+        alt="Loading..."
+        data-testid="test-loader-img"
+      />
     </div>
   );
 };
